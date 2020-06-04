1d0757f191c4a642e74c09b6b44cd0a068b3b69e
maksim.repin <repin.maksim@motorsport.com>
2020-06-04 18:20:54 +0300

    Cleaned up imports and variables

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React, { useEffect } from "react";
+
 import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
+import { getRates } from "./store/actions";
+
 import Chart from "./components/Chart/";
 import useInterval from "./hooks/useInterval";
-import { getRates } from "./store/actions";
 import Display from "./components/Display";
 import Error from "./components/Error";
 import Controls from "./components/Controls";
 
+import "./App.css";
+
 function App() {
   const dispatch = useDispatch();
   const { data, error, loading } = useSelector((state) => state);
+
   const USD = data[0].data[data[1].data.length - 1];
   const EUR = data[1].data[data[1].data.length - 1];
-  const handleGet = () => dispatch(getRates());
   const showChart = data[0].data.length > 0 && data[1].data.length > 0;
+
+  const handleGet = () => dispatch(getRates());
 
   useEffect(() => {
     handleGet();
